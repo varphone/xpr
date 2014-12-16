3d1fa23a61e6e43e9478bfaf72f8b209b953304b
Varphone Wong <varphone@qq.com>
2014-12-16 15:31:22 +0800

    Fix XPR_ADC dummy implements

--- src/xpr_adc.c
+++ src/xpr_adc.c
@@ -1,28 +1,49 @@
 #if !defined(XPR_ADC_DRIVER_A5S)
+#include <xpr/xpr_errno.h>
+#include <xpr/xpr_common.h>
 #include <xpr/xpr_adc.h>
+
+int XPR_ADC_Config(int cfg, const void* data, int length)
+{
+    return XPR_ERR_ERROR;
+}
 
 int XPR_ADC_Init(void)
 {
     //FIXME:
-    return -1;
+    return XPR_ERR_ERROR;
 }
 
-void XPR_ADC_Fini(void)
+int XPR_ADC_Fini(void)
 {
     //FIXME:
+    return XPR_ERR_ERROR;
 }
 
-int XPR_ADC_GetChannelCount(void)
+int XPR_ADC_IsPortReady(int port)
+{
+    return XPR_FALSE;
+}
+
+int XPR_ADC_IsPortValid(int port)
+{
+    return XPR_FALSE;
+}
+
+int XPR_ADC_GetValue(int port, void* buffer, int* size)
 {
     //FIXME:
-    return 0;
+    return XPR_ERR_ERROR;
 }
 
-int XPR_ADC_GetValue(int channel)
+int XPR_ADC_SetParam(int port, const void* data, int length)
 {
-    //FIXME:
-    return 0;
+    return XPR_ERR_ERROR;
 }
 
+int XPR_ADC_GetParam(int port, void* buffer, int* size)
+{
+    return XPR_ERR_ERROR;
+}
 #endif
 
