5d7b30718e297da7f541a523f47e4043377f3400
Varphone Wong <varphone@qq.com>
2015-01-13 11:14:12 +0800

    Update json testing

--- tests/json.c
+++ tests/json.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
 #include <xpr/xpr_json.h>
 
+static void print_indent(int n)
+{
+    putchar('|');
+    while (--n > 0)
+        putchar('-');
+}
+
+static void dump(XPR_JSON* jo)
+{
+    static int indent = 0;
+    XPR_JSON* jn = 0;
+    void* itr = XPR_JSON_ObjectIter(jo);
+    indent += 4;
+    while (itr) {
+        print_indent(indent);
+        printf("%s\n", XPR_JSON_ObjectIterKey(itr));
+        jn = XPR_JSON_ObjectIterValue(itr);
+        if (jn)
+            dump(jn);
+        itr = XPR_JSON_ObjectIterNext(jo, itr);
+    }
+    indent -= 4;
+}
+
 int main(int argc, char* argv[])
 {
-	XPR_JSON* root_json = XPR_JSON_LoadFileName("./configuration.json");
-	/*printf("root_json=%s\n",  XPR_JSON_DumpString(root_json));
-	XPR_JSON* js = XPR_JSON_ObjectGet(root_json, "/");
-	printf("js_json=%s\n",  XPR_JSON_DumpString(js));
-	js = XPR_JSON_ObjectGet(js, "system");
-	printf("js_json=%s\n",  XPR_JSON_DumpString(js));
-	XPR_JSON_DumpFileName(root_json, "./test.json");
-*/
-	XPR_JSON_DecRef(root_json);
-	return 0;
+    XPR_JSON* root_json = XPR_JSON_LoadFileName("./configuration.json");
+    dump(root_json);
+    XPR_JSON_DecRef(root_json);
+    return 0;
 }
