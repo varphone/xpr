f87c84be81cad96224280f081e8ec7d0a8937c5f
黄华锋 <varphone@qq.com>
2016-12-13 10:59:15 +0800

    sha1: add extern "C" for C++

--- include/xpr/xpr_sha1.h
+++ include/xpr/xpr_sha1.h
@@ -10,27 +10,35 @@
 #include <stdint.h>
 #include <xpr/xpr_common.h>
 
-/// @brief SHA1 哈希值字串缓冲区长度
-#define XPR_SHA1_HASH_BUF	42
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
 
-typedef struct {
-    uint32_t state[5];
-    uint32_t count[2];
-    uint8_t buffer[64];
-} XPR_SHA1_CTX;
+	/// @brief SHA1 哈希值字串缓冲区长度
+	#define XPR_SHA1_HASH_BUF	42
 
-XPR_API void XPR_SHA1Transform(uint32_t state[5], const uint8_t buffer[64]);
+	typedef struct {
+		uint32_t state[5];
+		uint32_t count[2];
+		uint8_t buffer[64];
+	} XPR_SHA1_CTX;
 
-XPR_API void XPR_SHA1Init(XPR_SHA1_CTX* context);
+	XPR_API void XPR_SHA1Transform(uint32_t state[5], const uint8_t buffer[64]);
 
-XPR_API void XPR_SHA1Update(XPR_SHA1_CTX* context, const uint8_t *data, size_t length);
+	XPR_API void XPR_SHA1Init(XPR_SHA1_CTX* context);
 
-XPR_API void XPR_SHA1Final(XPR_SHA1_CTX* context, uint8_t digest[20]);
+	XPR_API void XPR_SHA1Update(XPR_SHA1_CTX* context, const uint8_t *data, size_t length);
 
-/// @note `hashOut` must be large than XPR_SHA1_HASH_BUF bytes
-XPR_API char* XPR_SHA1Data(const uint8_t* data, size_t length, char* hashOut);
+	XPR_API void XPR_SHA1Final(XPR_SHA1_CTX* context, uint8_t digest[20]);
 
-/// @note `hashOut` must be large than XPR_SHA1_HASH_BUF bytes
-XPR_API char* XPR_SHA1File(const char* path, char* hashOut);
+	/// @note `hashOut` must be large than XPR_SHA1_HASH_BUF bytes
+	XPR_API char* XPR_SHA1Data(const uint8_t* data, size_t length, char* hashOut);
+
+	/// @note `hashOut` must be large than XPR_SHA1_HASH_BUF bytes
+	XPR_API char* XPR_SHA1File(const char* path, char* hashOut);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
 
 #endif /* XPR_SHA1_H */
