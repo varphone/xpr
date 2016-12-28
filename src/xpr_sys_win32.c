e0e4ed9d85a69e3ca2e7a3f4741a6b48a491be1b
Varphone Wong <varphone@qq.com>
2016-12-28 10:41:11 +0800

    Code beautify

--- src/xpr_sys_win32.c
+++ src/xpr_sys_win32.c
@@ -27,43 +27,42 @@ XPR_API void XPR_SYS_DelayedReboot(int secs)
 // FIXME
 XPR_API int XPR_SYS_EnableThreadRealtimeSchedule(int64_t tid)
 {
-    return XPR_ERR_GEN_NOT_SUPPORT;
+	return XPR_ERR_GEN_NOT_SUPPORT;
 }
 
 // FIXME
 XPR_API int XPR_SYS_EnableProcessRealtimeSchedule(int64_t pid)
 {
-    return XPR_ERR_GEN_NOT_SUPPORT;
+	return XPR_ERR_GEN_NOT_SUPPORT;
 }
 
 // FIXME
 XPR_API int XPR_SYS_SetAudioClockFrequency(int freq)
 {
-    return XPR_ERR_GEN_NOT_SUPPORT;
+	return XPR_ERR_GEN_NOT_SUPPORT;
 }
 
 
 typedef NTSTATUS(NTAPI* NtQueryPerformanceCounterProc)(
-    PLARGE_INTEGER PerformanceCounter,
-    PLARGE_INTEGER PerformanceFrequency);
+	PLARGE_INTEGER PerformanceCounter,
+	PLARGE_INTEGER PerformanceFrequency);
 
 static NtQueryPerformanceCounterProc ntqpcp = 0;
 
 XPR_API int64_t XPR_SYS_GetCTS(void)
 {
-    LARGE_INTEGER ticks;
-    LARGE_INTEGER freq;
-    if (!ntqpcp) {
-	ntqpcp = (NtQueryPerformanceCounterProc)GetProcAddress(
-		     GetModuleHandleA("ntdll.dll"),
-		     "NtQueryPerformanceCounter");
-    }
-    if (ntqpcp) {
-	ntqpcp(&ticks, &freq);
-    }
-    else {
-	QueryPerformanceFrequency(&freq);
-	QueryPerformanceCounter(&ticks);
-    }
-    return ticks.QuadPart / (freq.QuadPart / 1000000);
+	LARGE_INTEGER ticks;
+	LARGE_INTEGER freq;
+	if (!ntqpcp) {
+		ntqpcp = (NtQueryPerformanceCounterProc)GetProcAddress(
+				 GetModuleHandleA("ntdll.dll"), "NtQueryPerformanceCounter");
+	}
+	if (ntqpcp) {
+		ntqpcp(&ticks, &freq);
+	}
+	else {
+		QueryPerformanceFrequency(&freq);
+		QueryPerformanceCounter(&ticks);
+	}
+	return ticks.QuadPart / (freq.QuadPart / 1000000);
 }
