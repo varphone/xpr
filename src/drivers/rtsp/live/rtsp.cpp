b07414a0fe9ba65a8fba7576995ec58a10fbae6c
Varphone Wong <varphone@qq.com>
2019-04-20 09:27:49 +0800

    rtsp: live: Remove unused codes
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- src/drivers/rtsp/live/rtsp.cpp
+++ src/drivers/rtsp/live/rtsp.cpp
@@ -169,32 +169,6 @@ xpr::rtsp::Port* xpr::rtsp::PortManager::getMinorPort(int port)
     return mp ? mp->getPort(port) : NULL;
 }
 
-/*
-int xpr::rtsp::PortManager::init(void)
-{
-#if defined(HAVE_XPR_RTSP_CLIENT)
-    mMajorPorts[XPR_RTSP_PORT_MAJOR_CLI] = new xpr::rtsp::ClientManager();
-#endif // HAVE_XPR_RTSP_CLIENT
-#if defined(HAVE_XPR_RTSP_SERVER)
-    mMajorPorts[XPR_RTSP_PORT_MAJOR_SVR] = new xpr::rtsp::Server(XPR_RTSP_PORT_MAJOR_SVR, this);
-#endif // HAVE_XPR_RTSP_SERVER
-    return XPR_ERR_OK;
-}
-
-int xpr::rtsp::PortManager::fini(void)
-{
-#if defined(HAVE_XPR_RTSP_CLIENT)
-    delete mMajorPorts[XPR_RTSP_PORT_MAJOR_CLI];
-    mMajorPorts[XPR_RTSP_PORT_MAJOR_CLI] = NULL;
-#endif // HAVE_XPR_RTSP_CLIENT
-#if defined(HAVE_XPR_RTSP_SERVER)
-    delete mMajorPorts[XPR_RTSP_PORT_MAJOR_SVR];
-    mMajorPorts[XPR_RTSP_PORT_MAJOR_SVR] = NULL;
-#endif // HAVE_XPR_RTSP_SERVER
-    return XPR_ERR_OK;
-}
-*/
-
 int xpr::rtsp::PortManager::setupServer(const char* url)
 {
     // 创建服务器
@@ -240,163 +214,104 @@ XPR_API int XPR_RTSP_Init(void)
     rtsp = new xpr::rtsp::PortManager();
     if (rtsp == NULL)
         return XPR_ERR_GEN_NOMEM;
-    //return rtsp->init();
     return XPR_ERR_OK;
-    /*
-        memset(rtsp_ports, 0, sizeof(rtsp_ports));
-    #if defined(HAVE_XPR_RTSP_CLIENT)
-        rtsp_ports[XPR_RTSP_PORT_MAJOR_CLI] = new xpr::rtsp::ClientManager();
-    #endif // HAVE_XPR_RTSP_CLIENT
-    #if defined(HAVE_XPR_RTSP_SERVER)
-        rtsp_ports[XPR_RTSP_PORT_MAJOR_SVR] = new xpr::rtsp::ServerManager();
-    #endif // HAVE_XPR_RTSP_SERVER
-        return 0;
-    */
 }
 
 XPR_API int XPR_RTSP_Fini(void)
 {
     if (rtsp == NULL)
         return XPR_ERR_GEN_UNEXIST;
-    //int retval = rtsp->fini();
-    //if (retval != XPR_ERR_OK)
-    //  return retval;
     delete rtsp;
     rtsp = NULL;
     return XPR_ERR_OK;
-    /*
-    #if defined(HAVE_XPR_RTSP_CLIENT)
-        delete rtsp_ports[XPR_RTSP_PORT_MAJOR_CLI];
-        rtsp_ports[XPR_RTSP_PORT_MAJOR_CLI] = NULL;
-    #endif // HAVE_XPR_RTSP_CLIENT
-    #if defined(HAVE_XPR_RTSP_SERVER)
-        delete rtsp_ports[XPR_RTSP_PORT_MAJOR_SVR];
-        rtsp_ports[XPR_RTSP_PORT_MAJOR_SVR] = NULL;
-    #endif // HAVE_XPR_RTSP_SERVER
-        return 0;
-    */
 }
 
 XPR_API int XPR_RTSP_IsPortValid(int port)
 {
     return rtsp->isPortValid(port);
-    //xpr::rtsp::Port* p = xpr::rtsp::Port::getPort(port);
-    //return p ? (p->isPortValid(port) ? XPR_TRUE : XPR_FALSE) : XPR_FALSE;
 }
 
 XPR_API int XPR_RTSP_Open(int port, const char* url)
 {
     return rtsp->open(port, url);
-    //xpr::rtsp::Port* p = xpr::rtsp::Port::getPort(port);
-    //return p ? p->open(port, url) : XPR_ERR_GEN_NOT_SUPPORT;
 }
 
 XPR_API int XPR_RTSP_Close(int port)
 {
     return rtsp->close(port);
-    //xpr::rtsp::Port* p = xpr::rtsp::Port::getPort(port);
-    //return p ? p->close(port) : XPR_ERR_GEN_NOT_SUPPORT;
 }
 
 XPR_API int XPR_RTSP_Start(int port)
 {
     return rtsp->start(port);
-    //xpr::rtsp::Port* p = xpr::rtsp::Port::getPort(port);
-    //return p ? p->start(port) : XPR_ERR_GEN_NOT_SUPPORT;
 }
 
 XPR_API int XPR_RTSP_Stop(int port)
 {
     return rtsp->stop(port);
-    //xpr::rtsp::Port* p = xpr::rtsp::Port::getPort(port);
-    //return p ? p->stop(port) : XPR_ERR_GEN_NOT_SUPPORT;
 }
 
 XPR_API int XPR_RTSP_AddDataCallback(int port, XPR_RTSP_DCB cb, void* opaque)
 {
     return rtsp->addDataCallback(port, cb, opaque);
-    //xpr::rtsp::Port* p = xpr::rtsp::Port::getPort(port);
-    //return p ? p->addDataCallback(port, cb, opaque) : XPR_ERR_GEN_NOT_SUPPORT;
 }
 
 XPR_API int XPR_RTSP_DelDataCallback(int port, XPR_RTSP_DCB cb, void* opaque)
 {
     return rtsp->delDataCallback(port, cb, opaque);
-    //xpr::rtsp::Port* p = xpr::rtsp::Port::getPort(port);
-    //return p ? p->delDataCallback(port, cb, opaque) : XPR_ERR_GEN_NOT_SUPPORT;
 }
 
 XPR_API int XPR_RTSP_AddEventCallback(int port, XPR_RTSP_EVCB cb, void* opaque)
 {
     return rtsp->addEventCallback(port, cb, opaque);
-    //xpr::rtsp::Port* p = xpr::rtsp::Port::getPort(port);
-    //return p ? p->addEventCallback(port, cb, opaque) : XPR_ERR_GEN_NOT_SUPPORT;
 }
 
 XPR_API int XPR_RTSP_DelEventCallback(int port, XPR_RTSP_EVCB cb, void* opaque)
 {
     return rtsp->delEventCallback(port, cb, opaque);
-    //xpr::rtsp::Port* p = xpr::rtsp::Port::getPort(port);
-    //return p ? p->delEventCallback(port, cb, opaque) : XPR_ERR_GEN_NOT_SUPPORT;
 }
 
 XPR_API int XPR_RTSP_SetAuth(int port, const char* username,
                              const char* password, int pwdIsMD5)
 {
     return rtsp->setAuth(port, username, password, pwdIsMD5);
-    //xpr::rtsp::Port* p = xpr::rtsp::Port::getPort(port);
-    //return p ? p->setAuth(port, username, password, pwdIsMD5) : XPR_ERR_GEN_NOT_SUPPORT;
 }
 
 XPR_API int XPR_RTSP_SetOutputFormat(int port, int fourcc)
 {
     return rtsp->setOutputFormat(port, fourcc);
-    //xpr::rtsp::Port* p = xpr::rtsp::Port::getPort(port);
-    //return p ? p->setOutputFormat(port, fourcc) : XPR_ERR_GEN_NOT_SUPPORT;
 }
 
 XPR_API int XPR_RTSP_PushData(int port, XPR_StreamBlock* stb)
 {
     return rtsp->pushData(port, stb);
-    //xpr::rtsp::Port* p = xpr::rtsp::Port::getPort(port);
-    //return p ? p->postData(port, stb) : XPR_ERR_GEN_NOT_SUPPORT;
 }
 
 XPR_API int XPR_RTSP_PostData(int port, XPR_StreamBlock* stb)
 {
     return rtsp->postData(port, stb);
-    //xpr::rtsp::Port* p = xpr::rtsp::Port::getPort(port);
-    //return p ? p->postData(port, stb) : XPR_ERR_GEN_NOT_SUPPORT;
 }
 
 XPR_API int XPR_RTSP_PostEvent(int port, const XPR_RTSP_EVD* evd)
 {
     return rtsp->postEvent(port, evd);
-    //xpr::rtsp::Port* p = xpr::rtsp::Port::getPort(port);
-    //return p ? p->postEvent(port, evd) : XPR_ERR_GEN_NOT_SUPPORT;
 }
 
 XPR_API int XPR_RTSP_SetTrSpec(int port, XPR_RTSP_TRSPEC trspec)
 {
     return rtsp->setTrSpec(port, trspec);
-    //xpr::rtsp::Port* p = xpr::rtsp::Port::getPort(port);
-    //return p ? p->setTrSpec(port, trspec) : XPR_ERR_GEN_NOT_SUPPORT;
 }
 
 XPR_API int XPR_RTSP_GetParam(int port, XPR_RTSP_PARAM param, void* buffer,
                               int* size)
 {
     return rtsp->getParam(port, param, buffer, size);
-    //xpr::rtsp::Port* p = xpr::rtsp::Port::getPort(port);
-    //return p ? p->getParam(port, param, buffer, size) : XPR_ERR_GEN_NOT_SUPPORT;
 }
 
 XPR_API int XPR_RTSP_SetParam(int port, XPR_RTSP_PARAM param, const void* data,
                               int length)
 {
     return rtsp->setParam(port, param, data, length);
-    //xpr::rtsp::Port* p = xpr::rtsp::Port::getPort(port);
-    //return p ? p->setParam(port, param, data, length) : XPR_ERR_GEN_NOT_SUPPORT;
 }
 
 #endif // defined(HAVE_XPR_RTSP_DRIVER_LIVE)
