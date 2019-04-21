797b7617b89a9fcb4957e77d5b383d1fffebc531
Varphone Wong <varphone@qq.com>
2019-04-22 00:24:21 +0800

    benchmarks: rtsp-mgr-open-close: Use XPR_ERR_ASSERT() implements
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- benchmarks/rtsp-mgr-open-close.c
+++ benchmarks/rtsp-mgr-open-close.c
@@ -4,15 +4,23 @@
 #include <xpr/xpr_rtsp.h>
 #include <xpr/xpr_utils.h>
 
+#define XPR_ERR_ASSERT(err)                                                    \
+    if (err != XPR_ERR_OK) {                                                   \
+        printf("%s:%d, XPR_ErrorCode = 0x%08X\n", __FILE__, __LINE__, err);    \
+        abort();                                                               \
+    }
+
 void benchmark(void)
 {
     XPR_RTSP_Init();
+    int ret;
     int port = XPR_RTSP_PORT(XPR_RTSP_PORT_MAJOR_CLI, 0, 0);
     uint32_t n = 0;
     while (1) {
-        if (XPR_RTSP_Open(port, "dummy://any") == XPR_ERR_OK) {
-            XPR_RTSP_Close(port);
-        }
+        ret = XPR_RTSP_Open(port, "dummy://any?unknown");
+        XPR_ERR_ASSERT(ret);
+        ret = XPR_RTSP_Close(port);
+        XPR_ERR_ASSERT(ret);
         n++;
         if ((n % 1000) == 0)
             printf("Benchmark %u times\n", n);
