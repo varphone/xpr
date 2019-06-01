325c6e37e7d437c55d07b34bf200c5a6cceeefff
Varphone Wong <varphone@qq.com>
2019-06-01 11:04:20 +0800

    benchmarks: 新增 rtsp-conn-rx-264-with-log, rtsp-svr-tx-264-with-log 用于传输正确性诊断
    诊断方法：
    
    1. 运行 rtsp-svr-tx-264-with-log 准备发送，此程序启动后会等待用户输入按键事件才会开始发送数据。
    2. 运行 rtsp-conn-rx-264-with-log 开始接收数据，当此程序启动后，切到 rtsp-svr-tx-264-with-log
       的终端输入任意键开始发送数据。
    3. 等待数据发送完成。
    4. 通过比较当前目录下的 rtsp-svr-tx-264-with-log.264 <> rtsp-conn-rx-264-with-log.264,
       rtsp-svr-tx-264-with-log.txt <> rtsp-conn-rx-264-with-log.txt 诊断是否存在异常。
    
    其中发送的数据样本为：example.264.framed，可以将其内容替换为你想要测试的 264 数据。
    
    Signed-off-by: Varphone Wong <varphone@qq.com>

---
+++ benchmarks/rtsp-conn-rx-264-with-log.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <xpr/xpr_errno.h>
+#include <xpr/xpr_file.h>
+#include <xpr/xpr_rtsp.h>
+#include <xpr/xpr_thread.h>
+#include <xpr/xpr_utils.h>
+
+#define XPR_ERR_ASSERT(err)                                                    \
+    if (err != XPR_ERR_OK) {                                                   \
+        printf("%s:%d, XPR_ErrorCode = 0x%08X\n", __FILE__, __LINE__, err);    \
+        abort();                                                               \
+    }
+
+// NOTE:
+// The contents after "??" is use to config the connection working params.
+// - autoRestart: Enable auto restart when src stopped or timeouted
+// - restartDelay: Set auto restart delay in us
+// - rxTimeout: Set rtp receive timeout in us
+// - useFrameMerger: Enable frame merger on 264/265 fragments
+#define CONN_CFG                                                                \
+    "??"                                                                       \
+    "autoRestart=false&"                                                        \
+    "restartDelay=1000000&"                                                    \
+    "rxTimeout=10000000&"                                                       \
+    "useFrameMerger=true"
+
+const char* urls[] = {
+    "rtsp://127.0.0.1:8554/live/1" CONN_CFG,
+};
+
+const int numUrls = sizeof(urls) / sizeof(urls[0]);
+
+struct Context {
+    int port;
+    uint32_t frames;
+    uint32_t offset;
+    XPR_File* dataFile;
+    XPR_File* logFile;
+};
+
+static char logBuffer[1024];
+static char logHeader[] = " Index     Offset Length Header\n";
+
+static int data_handler(void* opaque, int port, const XPR_StreamBlock* stb)
+{
+    int n = 0;
+    struct Context* ctx = (struct Context*)opaque;
+    if (ctx && stb->codec == AV_FOURCC_H264) {
+        XPR_FileWrite(ctx->dataFile, stb->data, stb->dataSize);
+        n = sprintf(logBuffer, "%6d %10d %6d [%02X %02X %02X %02X %02X %02X]\n",
+                    ctx->frames, ctx->offset, stb->dataSize, stb->data[0],
+                    stb->data[1], stb->data[2], stb->data[3], stb->data[4],
+                    stb->data[5]);
+        XPR_FileWrite(ctx->logFile, logBuffer, n);
+        ctx->frames += 1;
+        ctx->offset += stb->dataSize;
+    }
+    printf("Port %x, codec %x, flags %x, size %d, pts %ld\n", port, stb->codec,
+           stb->flags, stb->dataSize, stb->pts);
+    return 0;
+}
+
+static int event_handler(void* opaque, int port, const XPR_RTSP_EVD* evd)
+{
+    if (evd) {
+        printf("Event: %d, ", evd->event);
+        if (evd->event == XPR_RTSP_EVT_TIMEOUT) {
+            printf("XPR_RTSP_EVT_TIMEOUT");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+void benchmark(void)
+{
+    int ret;
+    struct Context ctx = {-1, 0, 0, NULL, NULL};
+
+    ctx.dataFile = XPR_FileOpen("rtsp-conn-rx-264-with-log.264", "cwb");
+    ctx.logFile = XPR_FileOpen("rtsp-conn-rx-264-with-log.txt", "cwb");
+    ret = sprintf(logBuffer, "%s", logHeader);
+    XPR_FileWrite(ctx.logFile, logBuffer, ret);
+
+    // Initialize the XPR_RTSP module
+    XPR_RTSP_Init();
+
+    // Open and start the rtsp manager
+    int mgrPort = XPR_RTSP_PORT(XPR_RTSP_PORT_MAJOR_CLI, 0, 0);
+    ret = XPR_RTSP_Open(mgrPort, "dummy://any");
+    XPR_ERR_ASSERT(ret);
+    ret = XPR_RTSP_Start(mgrPort);
+    XPR_ERR_ASSERT(ret);
+
+    // Open and start the rtsp connection
+    ctx.port = XPR_RTSP_PORT(XPR_RTSP_PORT_MAJOR_CLI, 1, 0);
+    ret = XPR_RTSP_Open(ctx.port , urls[0]);
+    XPR_ERR_ASSERT(ret);
+    XPR_RTSP_AddDataCallback(ctx.port , data_handler, (void*)&ctx);
+    XPR_RTSP_AddEventCallback(ctx.port, event_handler, (void*)&ctx);
+    XPR_RTSP_SetAuth(ctx.port , "admin", "123456", 0);
+    ret = XPR_RTSP_Start(ctx.port);
+    XPR_ERR_ASSERT(ret);
+
+    printf("Press any key to stop ...\n");
+    getchar();
+
+    XPR_FileFlush(ctx.dataFile);
+    XPR_FileFlush(ctx.logFile);
+
+    // Stop and close rtsp connection(s)
+    ret = XPR_RTSP_Stop(ctx.port);
+    XPR_ERR_ASSERT(ret);
+    ret = XPR_RTSP_Close(ctx.port);
+    XPR_ERR_ASSERT(ret);
+
+    // Stop and close rtsp manager
+    ret = XPR_RTSP_Stop(mgrPort);
+    XPR_ERR_ASSERT(ret);
+    ret = XPR_RTSP_Close(mgrPort);
+    XPR_ERR_ASSERT(ret);
+
+    // Cleanup the XPR_RTSP module
+    XPR_RTSP_Fini();
+
+    XPR_FileClose(ctx.dataFile);
+    XPR_FileClose(ctx.logFile);
+}
---
+++ benchmarks/rtsp-svr-tx-264-with-log.c
@@ -0,0 +1,116 @@
+﻿#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <xpr/xpr_errno.h>
+#include <xpr/xpr_file.h>
+#include <xpr/xpr_rtsp.h>
+#include <xpr/xpr_thread.h>
+#include <xpr/xpr_utils.h>
+
+#define XPR_ERR_ASSERT(err)                                                    \
+    if (err != XPR_ERR_OK) {                                                   \
+        printf("%s:%d, XPR_ErrorCode = 0x%08X\n", __FILE__, __LINE__, err);    \
+        abort();                                                               \
+    }
+
+const char* exampleFileName = "example.264.framed";
+static char logBuffer[1024];
+static char logHeader[] = " Index     Offset Length Header\n";
+
+void benchmark(void)
+{
+    XPR_RTSP_Init();
+    // Open and start the server port first
+    int serverPort = XPR_RTSP_PORT(XPR_RTSP_PORT_MAJOR_SVR, 0, 0);
+    int err = XPR_RTSP_Open(
+        serverPort, "rtsp://0.0.0.0:8554/live/"
+                    "0?maxStreams=16&maxStreamTracks=4&maxWorkers=1&high");
+    XPR_ERR_ASSERT(err);
+    err = XPR_RTSP_Start(serverPort);
+    XPR_ERR_ASSERT(err);
+    // Open and start stream port after server started
+    int streamPort = XPR_RTSP_PORT(XPR_RTSP_PORT_MAJOR_SVR, 1, 0);
+    // Query:
+    // - track - Index of the track, must be first param of each track
+    // - mime - Track data mime type, like: audio/AAC, video/H264
+    err = XPR_RTSP_Open(streamPort, "uri:///live/1"
+                                    "?track=1&mime=video/H264");
+    XPR_ERR_ASSERT(err);
+    fprintf(stderr, "RTSP [%d] URL: %s\n", streamPort,
+            "rtsp://0.0.0.0:8554/live/1");
+    err = XPR_RTSP_Start(streamPort);
+    XPR_ERR_ASSERT(err);
+    // Open example file
+    XPR_File* f1 = XPR_FileOpen(exampleFileName, "rb");
+    if (XPR_FIEL_IS_NULL(f1)) {
+        fprintf(stderr, "Open [%s] failed, errno: %d\n", exampleFileName,
+                errno);
+        abort();
+    }
+    // Prepare logging files
+    XPR_File* dataFile = XPR_FileOpen("rtsp-svr-tx-264-with-log.264", "cwb");
+    XPR_File* logFile = XPR_FileOpen("rtsp-svr-tx-264-with-log.txt", "cwb");
+    int n = sprintf(logBuffer, "%s", logHeader);
+    XPR_FileWrite(logFile, logBuffer, n);
+    // Allocate stream block and set public fileds
+    XPR_StreamBlock stb;
+    memset(&stb, 0, sizeof(stb));
+    stb.bufferSize = 1024 * 1024;
+    stb.buffer = malloc(stb.bufferSize);
+    stb.data = stb.buffer;
+    stb.dataSize = 0;
+    stb.codec = AV_FOURCC_H264;
+    stb.track = 1;
+    // Timestamp accumulator
+    int64_t pts = 0;
+    // Logging accumulators
+    uint32_t frames = 0;
+    uint32_t offset = 0;
+    printf("Press any key to start ...\n");
+    getchar();
+    // Loop for read and send frame
+    while (1) {
+        // Read frame size
+        uint32_t frameSize = 0;
+        int n = XPR_FileRead(f1, (uint8_t*)&frameSize, sizeof(frameSize));
+        if (n != sizeof(frameSize)) {
+            break;
+        }
+        // Check frame size
+        if (frameSize <= 0)
+            break;
+        // Read frame content
+        n = XPR_FileRead(f1, stb.data, frameSize);
+        if (n != frameSize)
+            break;
+        // Update stream block
+        stb.dataSize = frameSize;
+        stb.dts = stb.pts = pts;
+        // Push to the server
+        XPR_RTSP_PushData(streamPort, &stb);
+        // Logging
+        XPR_FileWrite(dataFile, stb.data, stb.dataSize);
+        n = sprintf(logBuffer, "%6d %10d %6d [%02X %02X %02X %02X %02X %02X]\n",
+                    frames, offset, stb.dataSize, stb.data[0], stb.data[1],
+                    stb.data[2], stb.data[3], stb.data[4], stb.data[5]);
+        XPR_FileWrite(logFile, logBuffer, n);
+        frames += 1;
+        offset += stb.dataSize;
+        // 40000 us per frame for 25 fps
+        XPR_ThreadSleep(40000);
+        pts += 40000;
+    }
+    // Flush to disk
+    XPR_FileFlush(dataFile);
+    XPR_FileClose(dataFile);
+    XPR_FileFlush(logFile);
+    XPR_FileClose(logFile);
+    // Stop and close stream port first
+    XPR_RTSP_Stop(streamPort);
+    XPR_RTSP_Close(streamPort);
+    // Stop server port after all stream closed
+    XPR_RTSP_Stop(serverPort);
+    XPR_RTSP_Close(serverPort);
+    XPR_RTSP_Fini();
+}
