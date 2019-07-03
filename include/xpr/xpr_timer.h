8cc7480837b631e22f8c43aea41af3ba06f200d2
Varphone Wong <varphone@qq.com>
2019-07-03 16:02:40 +0800

    xpr: Timer: 更新功能实现
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- include/xpr/xpr_timer.h
+++ include/xpr/xpr_timer.h
@@ -6,7 +6,7 @@
  * Project       : xpr
  * Author        : Varphone Wong <varphone@qq.com>
  * File Created  : 2019-07-02 22:22:48 +08:00 Tuesday, 02 July
- * Last Modified : 2019-07-02 22:30:40 +08:00 Tuesday, 02 July
+ * Last Modified : 2019-07-03 22:30:40 +08:00 Wednesday, 03 July
  * Modified By   : Varphone Wong <varphone@qq.com>
  * ---------------------------------------------------------------------------
  * Copyright (C) 2010 - 2019 Varphone Wong, Varphone.com
@@ -33,6 +33,7 @@ typedef struct XPR_Timer XPR_Timer;
 #ifndef XPR_TIMER_ID_TYPE_DEFINED
 #define XPR_TIMER_ID_TYPE_DEFINED
 typedef int XPR_TimerId;
+#define XPR_TIMER_ID_SINGLESHOT 0xFFFFFFFF
 #endif // XPR_TIMER_TYPE_DEFINED
 
 #ifndef XPR_TIMERQUEUE_TYPE_DEFINED
@@ -43,10 +44,11 @@ typedef struct XPR_TimerQueue XPR_TimerQueue;
 
 #ifndef XPR_TIMER_RETURN_TYPE_DEFINED
 #define XPR_TIMER_RETURN_TYPE_DEFINED
-typedef enum XPR_TimerReturn {
-    XPR_TIMER_CONTINUE,
-    XPR_TIMER_SUSPEND,
-    XPR_TIMER_TERMINATE,
+typedef enum XPR_TimerReturn
+{
+    XPR_TIMER_CONTINUE,  ///< Reset the timer for next time
+    XPR_TIMER_SUSPEND,   ///< Put timer in suspend state
+    XPR_TIMER_TERMINATE, ///< Terminate the timer now
 } XPR_TimerReturn;
 #endif // XPR_TIMER_RETURN_TYPE_DEFINED
 
@@ -55,26 +57,100 @@ typedef enum XPR_TimerReturn {
 /// \return sa #XPR_TimerReturn
 typedef XPR_TimerReturn (*XPR_TimerFn)(void* opaque);
 
+/// Create a new timer with disabled initialize state.
+/// \param [in] id          To identify the timer, must be unique to each timers.
+/// \param [in] interval    Trigger interval in us.
+/// \param [in] fn          Function to callack if triggered.
+/// \param [in] opaque      User context data for callback.
+/// \note The XPR_TimerId == 0xFFFFFFFF is used for Single Shot Timers.
 XPR_API XPR_Timer* XPR_TimerCreate(XPR_TimerId id, int64_t interval,
-                                   XPR_TimerFn, void* opaque);
+                                   XPR_TimerFn fn, void* opaque);
 
+/// Destroy the timer and remove from the timer queue if added.
 XPR_API void XPR_TimerDestroy(XPR_Timer* self);
 
+/// Enable the timer and resume from the timer queue if added.
 XPR_API void XPR_TimerEnable(XPR_Timer* self);
 
+/// Disable the timer and suspend from the timer queue if added.
 XPR_API void XPR_TimerDisable(XPR_Timer* self);
 
+/// Return the id of the timer.
+XPR_API XPR_TimerId XPR_TimerGetId(XPR_Timer* self);
+
+/// Return the interval of the timer.
+XPR_API int64_t XPR_TimerGetInterval(XPR_Timer* self);
+
+// Return XPR_TRUE if the timer enabled, otherwise return XPR_FALSE.
+XPR_API int XPR_TimerIsEnabled(XPR_Timer* self);
+
+/// Set the timer inteval in us.
 XPR_API void XPR_TimerSetInterval(XPR_Timer* self, int64_t interval);
 
+/// Signal the TimerQueue destroy the Timer on last expired.
+XPR_API void XPR_TimerTerminate(XPR_Timer* self);
+
+/// Create a new timer queue.
 XPR_API XPR_TimerQueue* XPR_TimerQueueCreate(void);
 
+/// Destroy the time queue.
 XPR_API void XPR_TimerQueueDestroy(XPR_TimerQueue* self);
 
+/// Add timer to the timer queue.
+/// \retval XPR_ERR_OK      Success
+/// \retval Others          Error
 XPR_API int XPR_TimerQueueAdd(XPR_TimerQueue* self, XPR_Timer* timer);
 
-XPR_API XPR_Timer* XPR_TimerQueueFind(XPR_TimerQueue* self, XPR_TimerId id);
+/// Create a timer and add to the timer queue.
+/// \retval XPR_ERR_OK      Success
+/// \retval Others          Error
+XPR_API int XPR_TimerQueueAddNew(XPR_TimerQueue* self, XPR_TimerId id,
+                                 int64_t interval, XPR_TimerFn fn,
+                                 void* opaque);
 
+/// Return the id matched timer from the timer queue.
+XPR_API XPR_Timer* XPR_TimerQueueFindId(XPR_TimerQueue* self, XPR_TimerId id);
+
+/// Remove and destroy the timer from the timer queue.
+/// \retval XPR_ERR_OK      Success
+/// \retval Others          Error
 XPR_API int XPR_TimerQueueRemove(XPR_TimerQueue* self, XPR_Timer* timer);
+
+/// Remove and destroy the id matched timer from the timer queue.
+/// \retval XPR_ERR_OK      Success
+/// \retval Others          Error
+XPR_API int XPR_TimerQueueRemoveId(XPR_TimerQueue* self, XPR_TimerId id);
+
+/// Resume the timer from the timer queue.
+/// \retval XPR_ERR_OK      Success
+/// \retval Others          Error
+XPR_API int XPR_TimerQueueResume(XPR_TimerQueue* self, XPR_Timer* timer);
+
+/// Resume the id matched timer from the timer queue.
+/// \retval XPR_ERR_OK      Success
+/// \retval Others          Error
+XPR_API int XPR_TimerQueueResumeId(XPR_TimerQueue* self, XPR_TimerId id);
+
+/// Schedule a single shot timer to the timer queue.
+/// \note The timer will be remove and destroy after expired.
+/// \sa #XPR_TimerCreate
+/// \retval XPR_ERR_OK      Success
+/// \retval Others          Error
+XPR_API int XPR_TimerQueueSingleShot(XPR_TimerQueue* self, int64_t interval,
+                                     XPR_TimerFn fn, void* opaque);
+
+/// Suspend the timer from timer queue.
+/// \retval XPR_ERR_OK      Success
+/// \retval Others          Error
+XPR_API int XPR_TimerQueueSuspend(XPR_TimerQueue* self, XPR_Timer* timer);
+
+/// Suspend the id matched timer from timer queue.
+/// \retval XPR_ERR_OK      Success
+/// \retval Others          Error
+XPR_API int XPR_TimerQueueSuspendId(XPR_TimerQueue* self, XPR_TimerId id);
+
+/// Alias to #XPR_TimerQueueRemoveId
+#define XPR_TimerQueueTerminateId XPR_TimerQueueRemoveId
 
 #ifdef __cplusplus
 }
--- src/xpr_timer_unix.c
+++ src/xpr_timer_unix.c
@@ -23,31 +23,35 @@
 #include <xpr/xpr_mem.h>
 #include <xpr/xpr_sync.h>
 #include <xpr/xpr_sys.h>
-#include <xpr/xpr_timer.h>
 #include <xpr/xpr_thread.h>
+#include <xpr/xpr_timer.h>
+#include <xpr/xpr_utils.h>
 
 #define XPR_TIMER_FLAG_ENABLED 0x0001
-#define XPR_TIMER_FLAG_TERMINATE 0x0002
+#define XPR_TIMER_FLAG_SINGLESHOT 0x0002
+#define XPR_TIMER_FLAG_TERMINATE 0x0004
+#define XPR_TIMER_TS_UNIT 1000000
 
 struct XPR_Timer
 {
-    XPR_ListNodeSL _base; // SinglyLinked list node base
-    XPR_TimerId id;       // Timer identifer
-    int64_t interval;     // Interval in us
-    XPR_TimerFn fn;       // Callback on expired
-    void* opaque;         // User context data for callback
-    int64_t expire;       // Expire timestamp
-    int flags;            // Flags to control the timer
+    XPR_ListNodeSL _base;   // SinglyLinked list node base
+    XPR_TimerId id;         // Timer identifer
+    int64_t interval;       // Interval in us
+    XPR_TimerFn fn;         // Callback on expired
+    void* opaque;           // User context data for callback
+    int64_t expire;         // Expire timestamp
+    int flags;              // Flags to control the timer
+    XPR_TimerQueue* timerQ; // TimerQueue reference
 };
 
 struct XPR_TimerQueue
 {
-    int exitLoop;
-    XPR_List* activeTimers;
-    XPR_List* suspendTimers;
-    pthread_mutex_t lock;
-    pthread_cond_t schedule;
-    XPR_Thread* thread;
+    int exitLoop;            // Exit thread loop
+    XPR_List* activeTimers;  // Actived timers
+    XPR_List* suspendTimers; // Suspended timers
+    pthread_mutex_t lock;    // Resource lock
+    pthread_cond_t cond;     // Schedule cond
+    XPR_Thread* thread;      // Thread object
 };
 
 static void* nodeAlloc(void)
@@ -69,12 +73,12 @@ static int nodeCompare(void* a, void* b)
     if (ta->expire < tb->expire)
         return -1;
     if (ta->expire > tb->expire)
-        return 0;
+        return 1;
     return 0;
 }
 
-XPR_API XPR_Timer* XPR_TimerCreate(XPR_TimerId id, int64_t interval, XPR_TimerFn fn,
-                                   void* opaque)
+XPR_API XPR_Timer* XPR_TimerCreate(XPR_TimerId id, int64_t interval,
+                                   XPR_TimerFn fn, void* opaque)
 {
     XPR_Timer* timer = (XPR_Timer*)nodeAlloc();
     if (timer) {
@@ -82,26 +86,62 @@ XPR_API XPR_Timer* XPR_TimerCreate(XPR_TimerId id, int64_t interval, XPR_TimerFn
         timer->interval = interval;
         timer->fn = fn;
         timer->opaque = opaque;
-        timer->flags |= XPR_TIMER_FLAG_ENABLED;
+        timer->flags = 0;
+        timer->timerQ = NULL;
     }
     return timer;
 }
 
 XPR_API void XPR_TimerDestroy(XPR_Timer* self)
 {
-    nodeFree(self);
+    if (self) {
+        int err = XPR_ERR_ERROR;
+        if (self->timerQ)
+            err = XPR_TimerQueueRemove(self->timerQ, self);
+        if (err != XPR_ERR_OK)
+            nodeFree(self);
+    }
 }
 
 XPR_API void XPR_TimerEnable(XPR_Timer* self)
 {
-    if (self)
-        self->flags |= XPR_TIMER_FLAG_ENABLED;
+    if (self && !(self->flags & XPR_TIMER_FLAG_ENABLED)) {
+        if (self->timerQ)
+            XPR_TimerQueueResume(self->timerQ, self);
+        else
+            self->flags |= XPR_TIMER_FLAG_ENABLED;
+    }
 }
 
 XPR_API void XPR_TimerDisable(XPR_Timer* self)
 {
-    if (self)
-        self->flags &= ~XPR_TIMER_FLAG_ENABLED;
+    if (self && (self->flags & XPR_TIMER_FLAG_ENABLED)) {
+        if (self->timerQ)
+            XPR_TimerQueueSuspend(self->timerQ, self);
+        else
+            self->flags &= ~XPR_TIMER_FLAG_ENABLED;
+    }
+}
+
+XPR_API XPR_TimerId XPR_TimerGetId(XPR_Timer* self)
+{
+    if (!self)
+        return 0;
+    return self->id;
+}
+
+XPR_API int64_t XPR_TimerGetInterval(XPR_Timer* self)
+{
+    if (!self)
+        return 0;
+    return self->interval;
+}
+
+XPR_API int XPR_TimerIsEnabled(XPR_Timer* self)
+{
+    if (!self)
+        return XPR_ERR_GEN_NULL_PTR;
+    return (self->flags & XPR_TIMER_FLAG_ENABLED) ? XPR_TRUE : XPR_FALSE;
 }
 
 XPR_API void XPR_TimerSetInterval(XPR_Timer* self, int64_t interval)
@@ -110,23 +150,23 @@ XPR_API void XPR_TimerSetInterval(XPR_Timer* self, int64_t interval)
         self->interval = interval;
 }
 
-static int64_t timespecInUs(void)
+XPR_API void XPR_TimerTerminate(XPR_Timer* self)
 {
-    int64_t us = 0;
-    struct timespec ts;
-    clock_gettime(CLOCK_REALTIME, &ts);
-    us = ts.tv_sec;
-    us *= 1000000;
-    us += ts.tv_nsec / 1000;
-    return us;
+    if (self)
+        self->flags |= XPR_TIMER_FLAG_TERMINATE;
 }
 
-// Run timer action
 static inline XPR_TimerReturn timerExec(XPR_Timer* timer)
 {
+    if (timer->flags & XPR_TIMER_FLAG_TERMINATE)
+        return XPR_TIMER_TERMINATE;
     XPR_TimerReturn ret = timer->fn(timer->opaque);
-    if (ret == XPR_TIMER_CONTINUE)
-        timer->expire = timespecInUs() + timer->interval;
+    if (ret == XPR_TIMER_CONTINUE) {
+        // Reset the expire
+        timer->expire = XPR_SYS_GetCTS() + timer->interval;
+    }
+    if (timer->flags & XPR_TIMER_FLAG_SINGLESHOT)
+        ret = XPR_TIMER_TERMINATE;
     return ret;
 }
 
@@ -134,58 +174,50 @@ static void* timerQueueLoop(void* opaque, XPR_Thread* thread)
 {
     XPR_TimerQueue* self = (XPR_TimerQueue*)(opaque);
     XPR_Timer* timer = NULL;
-    struct timespec ts;
-    /* We allow thread cancellation at any time */
+    XPR_Timer* nextTimer = NULL;
+    // We allow thread cancellation at any time
     pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
     pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
     while (!self->exitLoop) {
         pthread_mutex_lock(&self->lock);
-        /* Get first event */
         timer = XPR_ListFirstNl(self->activeTimers);
-        /*
-         * If we have timers in queue, we setup a timer to wait for first one.
-         * In all cases, thread is woken up when a reschedule occurs.
-         */
+        // If timer was setted, wait to cond
         if (timer) {
-            ts.tv_sec = timer->expire / 1000000;
-            ts.tv_nsec = (timer->expire % 1000000) * 1000;
-            pthread_cond_timedwait(&self->schedule, &self->lock, &ts);
+            struct timespec ts;
+            ts.tv_sec = timer->expire / XPR_TIMER_TS_UNIT;
+            ts.tv_nsec = (timer->expire % XPR_TIMER_TS_UNIT) * 1000;
+            pthread_cond_timedwait(&self->cond, &self->lock, &ts);
         }
         else {
-            /* We just wait for reschedule since we don't have any timer */
-            pthread_cond_wait(&self->schedule, &self->lock);
+            // We just wait for recond since we don't have any timer
+            pthread_cond_wait(&self->cond, &self->lock);
         }
-        /* We need to check "running" flags to know if we must stop */
+        // We need to check "running" flags to know if we must stop
         if (self->exitLoop) {
             pthread_mutex_unlock(&self->lock);
             break;
         }
-        /*
-         * Now, we need to find why we were woken up. So, we compare current
-         * time with first timer to see if we must execute action associated
-         * with it.
-         */
-        int64_t now = timespecInUs();
-        /* Get first event */
+        // Inspect all expired timers
         timer = XPR_ListFirstNl(self->activeTimers);
-        /* If there is nothing to do for now, wait again */
-        if ((timer == NULL) || (timer->expire > now)) {
-            pthread_mutex_unlock(&self->lock);
-            continue;
+        while (timer) {
+            if (timer->expire > XPR_SYS_GetCTS())
+                break;
+            nextTimer = XPR_ListNextNl(self->activeTimers, timer);
+            XPR_ListTakeNl(self->activeTimers, timer);
+            if (timer->flags & XPR_TIMER_FLAG_ENABLED) {
+                XPR_TimerReturn tr = timerExec(timer);
+                if (tr == XPR_TIMER_CONTINUE)
+                    XPR_ListAppend(self->activeTimers, timer);
+                else if (tr == XPR_TIMER_SUSPEND)
+                    XPR_ListAppend(self->suspendTimers, timer);
+                else
+                    XPR_ListFree(self->activeTimers, timer);
+            }
+            else {
+                XPR_ListAppend(self->suspendTimers, timer);
+            }
+            timer = nextTimer;
         }
-        /*
-         * We have a timer to manage. Remove it from queue and mark it as
-         * running.
-         */
-        XPR_ListTakeNl(self->activeTimers, timer);
-        /* Execute user function and reschedule timer if required */
-        XPR_TimerReturn tr = timerExec(timer);
-        if (tr == XPR_TIMER_CONTINUE)
-            XPR_ListAppend(self->activeTimers, timer);
-        else if (tr == XPR_TIMER_SUSPEND)
-            XPR_ListAppend(self->suspendTimers, timer);
-        else
-            nodeFree(timer);
         pthread_mutex_unlock(&self->lock);
     }
     return NULL;
@@ -199,11 +231,14 @@ XPR_API XPR_TimerQueue* XPR_TimerQueueCreate(void)
                                             nodeFree, nodeCompare);
         self->suspendTimers = XPR_ListCreate(XPR_LIST_SINGLY_LINKED, nodeAlloc,
                                              nodeFree, nodeCompare);
-        pthread_mutexattr_t attr;
-        pthread_mutexattr_init(&attr);
-        pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
-        pthread_mutex_init(&self->lock, &attr);
-        pthread_cond_init(&self->schedule, NULL);
+        pthread_mutexattr_t mutexAttr;
+        pthread_mutexattr_init(&mutexAttr);
+        pthread_mutexattr_settype(&mutexAttr, PTHREAD_MUTEX_RECURSIVE);
+        pthread_mutex_init(&self->lock, &mutexAttr);
+        pthread_condattr_t condAttr;
+        pthread_condattr_init(&condAttr);
+        pthread_condattr_setclock(&condAttr, CLOCK_MONOTONIC);
+        pthread_cond_init(&self->cond, &condAttr);
         self->thread = XPR_ThreadCreate(timerQueueLoop, 0, self);
     }
     return self;
@@ -212,16 +247,24 @@ XPR_API XPR_TimerQueue* XPR_TimerQueueCreate(void)
 XPR_API void XPR_TimerQueueDestroy(XPR_TimerQueue* self)
 {
     if (self) {
+        // Signal the thread exit
         pthread_mutex_lock(&self->lock);
         self->exitLoop = 1;
-        pthread_cond_signal(&self->schedule);
+        pthread_cond_signal(&self->cond);
+        pthread_mutex_unlock(&self->lock);
+        // Wait for thread exited
         XPR_ThreadJoin(self->thread);
         XPR_ThreadDestroy(self->thread);
+        // Destroy all timers
+        pthread_mutex_lock(&self->lock);
         XPR_ListDestroy(self->activeTimers);
         XPR_ListDestroy(self->suspendTimers);
         pthread_mutex_unlock(&self->lock);
+        // Destroy the lock and cond
         pthread_mutex_destroy(&self->lock);
-        pthread_cond_destroy(&self->schedule);
+        pthread_cond_destroy(&self->cond);
+        // Destroy self
+        XPR_Free(self);
     }
 }
 
@@ -231,39 +274,138 @@ XPR_API int XPR_TimerQueueAdd(XPR_TimerQueue* self, XPR_Timer* timer)
         return XPR_ERR_GEN_NULL_PTR;
     int err = XPR_ERR_OK;
     pthread_mutex_lock(&self->lock);
+    timer->timerQ = self;
+    timer->expire = XPR_SYS_GetCTS() + timer->interval;
     if (timer->flags & XPR_TIMER_FLAG_ENABLED)
         err = XPR_ListAppendNl(self->activeTimers, timer);
     else
         err = XPR_ListAppendNl(self->suspendTimers, timer);
+    if (err == XPR_ERR_OK)
+        pthread_cond_signal(&self->cond);
     pthread_mutex_unlock(&self->lock);
     return err;
 }
 
-XPR_API XPR_Timer* XPR_TimerQueueFind(XPR_TimerQueue* self, XPR_TimerId id)
+XPR_API int XPR_TimerQueueAddNew(XPR_TimerQueue* self, XPR_TimerId id,
+                                 int64_t interval, XPR_TimerFn fn, void* opaque)
 {
+    int err = XPR_ERR_ERROR;
+    XPR_Timer* timer = XPR_TimerCreate(id, interval, fn, opaque);
+    if (!timer)
+        return XPR_ERR_GEN_NOMEM;
+    return XPR_TimerQueueAdd(self, timer);
+}
+
+XPR_API XPR_Timer* XPR_TimerQueueFindId(XPR_TimerQueue* self, XPR_TimerId id)
+{
+    if (!self || id == XPR_TIMER_ID_SINGLESHOT)
+        return NULL;
     XPR_Timer* timer = NULL;
     pthread_mutex_lock(&self->lock);
     timer = XPR_ListFirstNl(self->activeTimers);
-    while (timer->id != id) {
+    while (timer && timer->id != id) {
         timer = XPR_ListNextNl(self->activeTimers, timer);
     }
     if (timer == NULL) {
         timer = XPR_ListFirstNl(self->suspendTimers);
-        while (timer->id != id) {
+        while (timer && timer->id != id) {
             timer = XPR_ListNextNl(self->suspendTimers, timer);
         }
     }
+    // Don't exposure the timer if marked as TERMINATE
+    if (timer && timer->flags & XPR_TIMER_FLAG_TERMINATE)
+        timer = NULL;
     pthread_mutex_unlock(&self->lock);
     return timer;
 }
 
 XPR_API int XPR_TimerQueueRemove(XPR_TimerQueue* self, XPR_Timer* timer)
 {
+    if (!self || !timer)
+        return XPR_ERR_GEN_NULL_PTR;
     int err = XPR_ERR_OK;
     pthread_mutex_lock(&self->lock);
     err = XPR_ListRemoveNl(self->activeTimers, timer);
     if (err != XPR_ERR_OK)
         err = XPR_ListRemoveNl(self->suspendTimers, timer);
+    if (err == XPR_ERR_OK)
+        pthread_cond_signal(&self->cond);
     pthread_mutex_unlock(&self->lock);
     return err;
+}
+
+XPR_API int XPR_TimerQueueRemoveId(XPR_TimerQueue* self, XPR_TimerId id)
+{
+    int err = XPR_ERR_ERROR;
+    XPR_Timer* timer = XPR_TimerQueueFindId(self, id);
+    if (!timer)
+        return XPR_ERR_GEN_UNEXIST;
+    return XPR_TimerQueueRemove(self, timer);
+}
+
+XPR_API int XPR_TimerQueueResume(XPR_TimerQueue* self, XPR_Timer* timer)
+{
+    if (!self || !timer)
+        return XPR_ERR_GEN_NULL_PTR;
+    int err = XPR_ERR_OK;
+    pthread_mutex_lock(&self->lock);
+    err = XPR_ListTakeNl(self->suspendTimers, timer);
+    if (err == XPR_ERR_OK) {
+        timer->flags |= XPR_TIMER_FLAG_ENABLED;
+        XPR_ListAppendNl(self->activeTimers, timer);
+        pthread_cond_signal(&self->cond);
+    }
+    pthread_mutex_unlock(&self->lock);
+    return err;
+}
+
+XPR_API int XPR_TimerQueueResumeId(XPR_TimerQueue* self, XPR_TimerId id)
+{
+    int err = XPR_ERR_ERROR;
+    XPR_Timer* timer = XPR_TimerQueueFindId(self, id);
+    if (!timer)
+        return XPR_ERR_GEN_UNEXIST;
+    return XPR_TimerQueueResume(self, timer);
+}
+
+XPR_API int XPR_TimerQueueSingleShot(XPR_TimerQueue* self, int64_t interval,
+                                     XPR_TimerFn fn, void* opaque)
+{
+    if (!self || !fn)
+        return XPR_ERR_GEN_NULL_PTR;
+    int err = XPR_ERR_ERROR;
+    XPR_Timer* timer =
+        XPR_TimerCreate(XPR_TIMER_ID_SINGLESHOT, interval, fn, opaque);
+    if (timer) {
+        timer->flags |= XPR_TIMER_FLAG_ENABLED | XPR_TIMER_FLAG_SINGLESHOT;
+        err = XPR_TimerQueueAdd(self, timer);
+        if (err != XPR_ERR_OK)
+            XPR_TimerDestroy(timer);
+    }
+    return err;
+}
+
+XPR_API int XPR_TimerQueueSuspend(XPR_TimerQueue* self, XPR_Timer* timer)
+{
+    if (!self || !timer)
+        return XPR_ERR_GEN_NULL_PTR;
+    int err = XPR_ERR_OK;
+    pthread_mutex_lock(&self->lock);
+    err = XPR_ListTakeNl(self->activeTimers, timer);
+    if (err == XPR_ERR_OK) {
+        timer->flags &= ~XPR_TIMER_FLAG_ENABLED;
+        XPR_ListAppendNl(self->suspendTimers, timer);
+        pthread_cond_signal(&self->cond);
+    }
+    pthread_mutex_unlock(&self->lock);
+    return err;
+}
+
+XPR_API int XPR_TimerQueueSuspendId(XPR_TimerQueue* self, XPR_TimerId id)
+{
+    int err = XPR_ERR_ERROR;
+    XPR_Timer* timer = XPR_TimerQueueFindId(self, id);
+    if (!timer)
+        return XPR_ERR_GEN_UNEXIST;
+    return XPR_TimerQueueSuspend(self, timer);
 }
