4be19f52dc2f52d59c132e857328f40dc9980cb8
Varphone Wong <varphone@qq.com>
2019-07-07 16:48:04 +0800

    tests/ups: 更新测试例程
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- tests/ups.c
+++ tests/ups.c
@@ -1,96 +1,79 @@
-/* Note: 1.为保证库的绝对健壮性和稳定性，对库的实现必须添加单元测试,测试的格式和步骤及示例请参考test_ups_get_set_string
- *       2.对与最后一步为测试内存泄漏，我们使用工具来测试，代码里面用来测试一个正确的值被执行多次的示例. ***这一步必须要测试，防止以后混入大的项目无法测试内存问题***
- *       3.对于验收时，没有单元测试统一验收不合格，代码不允许合并到代码仓库.
- *		
-*/
-
-
+﻿/*
+ * File: ups.c
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  XPR-UPS 测试用例
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Project       : tests
+ * Author        : Varphone Wong <varphone@qq.com>
+ * File Created  : 2014-09-05 11:20:31 +08:00 Sunday, 05 Sept
+ * Last Modified : 2019-07-07 11:20:47 +08:00 Sunday, 07 July
+ * Modified By   : Varphone Wong <varphone@qq.com>
+ * ---------------------------------------------------------------------------
+ * Copyright (C) 2012 - 2019 CETC55, Technology Development CO.,LTD.
+ * Copyright (C) 2010 - 2019 Varphone Wong, Varphone.com
+ * All rights reserved.
+ * ---------------------------------------------------------------------------
+ * HISTORY:
+ * 2019-07-07   varphone    更新到最新版本
+ * 2014-09-05   李武祥      初始版本建立
+ */
 #include <stdio.h>
 #include <string.h>
 #include <xpr/xpr_ups.h>
+#include <xpr/xpr_utils.h>
 
-/*
-  @brief 单元测试字符串函数
-  @interface: XPR_UPS_GetString, XPR_UPS_SetString
-  @author: 李武祥
-  @date:  2014/09/05
-  @purpose： 测试目标函数在接受任何复杂参数的时候能够正常运行，会不会崩溃
-  @scene: 1.空字符串指针，或者接受缓存指针为空
-		  2.不存在的key
-		  3.超长的key
-		  4.接受的缓冲区偏小
-		  5.参数有效性检查，如IP地址是否是正确的
-		  6.循环测试5000次，查看是否有内存泄漏
-  @valgrind:
-			valgrind  ./ups
-			==8413== Memcheck, a memory error detector
-			==8413== Copyright (C) 2002-2013, and GNU GPL'd, by Julian Seward et al.
-			==8413== Using Valgrind-3.9.0 and LibVEX; rerun with -h for copyright info
-			==8413== Command: ./ups
-			==8413== 
-			场景[1]测试成功---
-			场景[2]测试成功---
-			场景[3]测试成功---
-			场景[4]测试成功---
-			场景[5]测试成功---
-			场景[6]测试成功---
-			==8413== 
-			==8413== HEAP SUMMARY:
-			==8413==     in use at exit: 0 bytes in 0 blocks
-			==8413==   total heap usage: 15,164 allocs, 15,164 frees, 409,492 bytes allocated
-			==8413== 
-			==8413== All heap blocks were freed -- no leaks are possible
-			==8413== 
-			==8413== For counts of detected and suppressed errors, rerun with: -v
-			==8413== ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)
-
-  @result: passed
-  @note: 额外的一些说明....
-*/
-
-int test_ups_get_set_string()
+int test_XPR_UPS_Get_Set_String()
 {
+    char value[128] = {0}, value1[8] = {0};
+    int results[6] = {0}, i = 0;
+    int size = sizeof(value);
 
-	char value[128] = {0},value1[8]={0};
-	int results[6]={0}, i=0;
-	int size = sizeof(value);
-	//测试场景1：空指针
-	results[0] = XPR_UPS_GetString(NULL, NULL, NULL);
+    // 测试场景1：空指针
+    results[0] = XPR_UPS_GetString(NULL, NULL, NULL);
 
-	//测试场景2：key名字输入错误
-	results[1] = XPR_UPS_GetString("/system/network/error/ipv4/address", value, &size);
+    // 测试场景2：key名字输入错误
+    results[1] =
+        XPR_UPS_GetString("/system/network/error/ipv4/address", value, &size);
 
-	//测试场景3：超级长的key.
-	results[2] = XPR_UPS_SetString("/system/sdsds/fdfdf/ggdgdg/gfgfg/ererer/asdasd/asdw/qweq/s/wq/as/wewq/qwe/qwe/network/eth0/ipv4/addresssdasd/asdasd", "192.168.1.22", strlen("192.168.1.22"));
+    // 测试场景3：超级长的key.
+    results[2] = XPR_UPS_SetString(
+        "/system/sdsds/fdfdf/ggdgdg/gfgfg/ererer/asdasd/asdw/qweq/s/wq/as/wewq/"
+        "qwe/qwe/network/eth0/ipv4/addresssdasd/asdasd",
+        "192.168.1.22", strlen("192.168.1.22"));
 
-	//测试场景4：接受的缓冲区偏小
-	size = sizeof(value1);
-	results[3] = XPR_UPS_GetString("/system/network/error/ipv4/address", value1, &size);
+    // 测试场景4：接受的缓冲区偏小
+    size = sizeof(value1);
+    results[3] =
+        XPR_UPS_GetString("/system/network/error/ipv4/address", value1, &size);
 
-	//测试场景5：参数有效性检查，检查是否是有效的IP地址
-	results[4] = XPR_UPS_SetString("/system/network/error/ipv4/address", "192.168.1.22.989", strlen("192.168.1.22.989"));
+    // 测试场景5：参数有效性检查，检查是否是有效的IP地址
+    results[4] =
+        XPR_UPS_SetString("/system/network/error/ipv4/address",
+                          "192.168.1.22.989", strlen("192.168.1.22.989"));
 
-	//测试场景6：循环测试
-	int count = 5000;
-	while(--count) {
-		size = sizeof(value);
-    	results[5] = XPR_UPS_GetString("/system/network/eth0/ipv4/address", value, &size);
-		if(results[5]!=0)
-			break;
-		results[5] = XPR_UPS_SetString("/system/network/eth0/ipv4/dns1", "192.168.0.4", strlen("192.168.0.4"));
-		if(results[5]!=0)
-			break;
-	}
-	for(i=0; i<6; i++) {
-		if(results[i]==0 && i!=5)
-			printf("场景[%d]测试失败!!!\n",(i+1));
-		else
-			printf("场景[%d]测试成功---\n",(i+1));
-	}
-	return 0;
+    // 测试场景6：循环测试
+    int count = 5000;
+    while (--count) {
+        size = sizeof(value);
+        results[5] = XPR_UPS_GetString("/system/network/eth0/ipv4/address",
+                                       value, &size);
+        if (results[5] != 0)
+            break;
+        results[5] = XPR_UPS_SetString("/system/network/eth0/ipv4/dns1",
+                                       "192.168.0.4", strlen("192.168.0.4"));
+        if (results[5] != 0)
+            break;
+    }
+    for (i = 0; i < 6; i++) {
+        if (results[i] == 0 && i != 5)
+            printf("场景[%d]测试失败!!!\n", (i + 1));
+        else
+            printf("场景[%d]测试成功---\n", (i + 1));
+    }
+    return 0;
 }
 
-void test_ups_set_sys_info()
+void test_XPR_UPS_Set_SystemInformation()
 {
     char* hardware = "v4.0.0";
     char* id = "sn-00000000";
@@ -111,234 +94,240 @@ void test_ups_set_sys_info()
     char* internal_model = "HD-25440-20D112000";
     char* serveradress = "0.japan.pool.ntp.org";
 
-    char value[128] = {0}; 
-	int size = sizeof(value);
-    if(XPR_UPS_SetString("/system/information/uri", uri, strlen(uri)) != 0)
+    char value[128] = {0};
+    int size = sizeof(value);
+    if (XPR_UPS_SetString("/system/information/uri", uri, strlen(uri)) != 0)
         printf("set info uri error!\n");
-    else 
+    else
         printf("set info uri ok!\n");
-    
-    if(XPR_UPS_SetString("/system/information/hardware", hardware, strlen(hardware)) != 0)
+
+    if (XPR_UPS_SetString("/system/information/hardware", hardware,
+                          strlen(hardware)) != 0)
         printf("set info hardware error!\n");
     else
         printf("set info hardware ok!\n");
 
-    if(XPR_UPS_SetString("/system/information/id", id, strlen(id)) != 0)
+    if (XPR_UPS_SetString("/system/information/id", id, strlen(id)) != 0)
         printf("set info id error!\n");
     else
         printf("set info hardware ok!\n");
 
-    if(XPR_UPS_SetString("/system/information/location", location, strlen(location)) != 0)
+    if (XPR_UPS_SetString("/system/information/location", location,
+                          strlen(location)) != 0)
         printf("set info location error!\n");
     else
         printf("set info location ok!\n");
 
-    if(XPR_UPS_SetString("/system/information/manufacturer", manufacturer, strlen(manufacturer)) != 0)
+    if (XPR_UPS_SetString("/system/information/manufacturer", manufacturer,
+                          strlen(manufacturer)) != 0)
         printf("set info manufacturer error!\n");
     else
         printf("set info manufacturer ok!\n");
 
-    if(XPR_UPS_SetString("/system/information/model", model, strlen(model)) != 0)
+    if (XPR_UPS_SetString("/system/information/model", model, strlen(model)) !=
+        0)
         printf("set info model error!\n");
     else
         printf("set info model ok!\n");
 
-    if(XPR_UPS_SetString("/system/information/name", name, strlen(name)) != 0)
+    if (XPR_UPS_SetString("/system/information/name", name, strlen(name)) != 0)
         printf("set info name error!\n");
     else
         printf("set info name ok!\n");
 
-    if(XPR_UPS_SetString("/system/information/onvif", onvif, strlen(onvif)) != 0)
+    if (XPR_UPS_SetString("/system/information/onvif", onvif, strlen(onvif)) !=
+        0)
         printf("set info onvif error!\n");
     else
         printf("set info onvif ok!\n");
 
-    if(XPR_UPS_SetString("/system/information/software", software, strlen(software)) != 0)
+    if (XPR_UPS_SetString("/system/information/software", software,
+                          strlen(software)) != 0)
         printf("set info software error!\n");
     else
         printf("set info software ok!\n");
 
-    if(XPR_UPS_SetString("/system/information/uuid", uuid, strlen(uuid)) != 0)
+    if (XPR_UPS_SetString("/system/information/uuid", uuid, strlen(uuid)) != 0)
         printf("set info uuid error!\n");
     else
         printf("set info uuid ok!\n");
 
-    if(XPR_UPS_SetString("/system/network/eth0/ipv4/address", ip, strlen(ip)) != 0)
+    if (XPR_UPS_SetString("/system/network/eth0/ipv4/address", ip,
+                          strlen(ip)) != 0)
         printf("set net address error!\n");
     else
         printf("set net address ok!\n");
 
-    if(XPR_UPS_SetString("/system/network/eth0/ipv4/netmask", netmask, strlen(netmask)) != 0)
+    if (XPR_UPS_SetString("/system/network/eth0/ipv4/netmask", netmask,
+                          strlen(netmask)) != 0)
         printf("set net netmask error!\n");
     else
         printf("set net netmask ok!\n");
 
-    if(XPR_UPS_SetString("/system/network/eth0/name", ethname, strlen(ethname)) != 0)
+    if (XPR_UPS_SetString("/system/network/eth0/name", ethname,
+                          strlen(ethname)) != 0)
         printf("set net eth0 name error!\n");
     else
         printf("set net eth0 name ok!\n");
 
-    if(XPR_UPS_SetString("/system/information/internal-model", internal_model, strlen(internal_model)) != 0)
+    if (XPR_UPS_SetString("/system/information/internal-model", internal_model,
+                          strlen(internal_model)) != 0)
         printf("set info internal-model error!\n");
     else
         printf("set info internal-model ok!\n");
 
-    if(XPR_UPS_SetString("/system/network/ntp/serveradress", serveradress, strlen(serveradress)) != 0)
+    if (XPR_UPS_SetString("/system/network/ntp/serveradress", serveradress,
+                          strlen(serveradress)) != 0)
         printf("set ntp serveradress error!\n");
     else
         printf("set ntp serveradress ok!\n");
 
-   if(XPR_UPS_SetString("/system/network/eth0/ipv4/dns1", dns1, strlen(dns1)) != 0)
+    if (XPR_UPS_SetString("/system/network/eth0/ipv4/dns1", dns1,
+                          strlen(dns1)) != 0)
         printf("set net dns1 error!\n");
     else
         printf("set net dns1 ok!\n");
 
-   if(XPR_UPS_SetString("/system/network/eth0/ipv4/dns2", dns2, strlen(dns2)) != 0)
+    if (XPR_UPS_SetString("/system/network/eth0/ipv4/dns2", dns2,
+                          strlen(dns2)) != 0)
         printf("set net dns2 error!\n");
     else
         printf("set net dns2 ok!\n");
 }
 
-void test_ups_get_sys_info()
+void test_XPR_UPS_Get_SystemInformation()
 {
+    char value[128] = {0};
+    int size = sizeof(value);
 
-    char value[128] = {0}; 
-	int size = sizeof(value);
-
-    if(XPR_UPS_GetString("/system/information/uri", value, &size) != 0)
+    if (XPR_UPS_GetString("/system/information/uri", value, &size) != 0)
         printf("get info uri error!\n");
-    else 
+    else
         printf("uri = %s\n", value);
-    
+
     size = sizeof(value);
-    if(XPR_UPS_GetString("/system/information/hardware", value, &size) != 0)
+    if (XPR_UPS_GetString("/system/information/hardware", value, &size) != 0)
         printf("get info hardware error!\n");
     else
         printf("hardware = %s\n", value);
 
     size = sizeof(value);
-    if(XPR_UPS_GetString("/system/information/id", value, &size) != 0)
+    if (XPR_UPS_GetString("/system/information/id", value, &size) != 0)
         printf("get info id error!\n");
     else
         printf("info id = %s\n", value);
 
     size = sizeof(value);
-    if(XPR_UPS_GetString("/system/information/location", value, &size) != 0)
+    if (XPR_UPS_GetString("/system/information/location", value, &size) != 0)
         printf("get info location error!\n");
     else
         printf("location = %s\n", value);
 
     size = sizeof(value);
-    if(XPR_UPS_GetString("/system/information/manufacturer", value, &size) != 0)
+    if (XPR_UPS_GetString("/system/information/manufacturer", value, &size) !=
+        0)
         printf("get info manufacturer error!\n");
     else
         printf("manufacturer = %s\n", value);
 
     size = sizeof(value);
-    if(XPR_UPS_GetString("/system/information/model", value, &size) != 0)
+    if (XPR_UPS_GetString("/system/information/model", value, &size) != 0)
         printf("get info model error!\n");
     else
         printf("model = %s\n", value);
 
     size = sizeof(value);
-    if(XPR_UPS_GetString("/system/information/name", value, &size) != 0)
+    if (XPR_UPS_GetString("/system/information/name", value, &size) != 0)
         printf("get info name error!\n");
     else
         printf("information name = %s\n", value);
 
     size = sizeof(value);
-    if(XPR_UPS_GetString("/system/information/onvif", value, &size) != 0)
+    if (XPR_UPS_GetString("/system/information/onvif", value, &size) != 0)
         printf("get info onvif error!\n");
     else
         printf("onvif = %s\n", value);
 
     size = sizeof(value);
-    if(XPR_UPS_GetString("/system/information/software", value, &size) != 0)
+    if (XPR_UPS_GetString("/system/information/software", value, &size) != 0)
         printf("get info software error!\n");
     else
         printf("software = %s\n", value);
 
     size = sizeof(value);
-    if(XPR_UPS_GetString("/system/information/uuid", value, &size) != 0)
+    if (XPR_UPS_GetString("/system/information/uuid", value, &size) != 0)
         printf("get info uuid error!\n");
     else
         printf("uuid = %s\n", value);
 
     size = sizeof(value);
-    if(XPR_UPS_GetString("/system/network/eth0/ipv4/address", value, &size) != 0)
+    if (XPR_UPS_GetString("/system/network/eth0/ipv4/address", value, &size) !=
+        0)
         printf("get net address error!\n");
     else
         printf("address = %s\n", value);
 
     size = sizeof(value);
-    if(XPR_UPS_GetString("/system/network/eth0/ipv4/netmask", value, &size) != 0)
+    if (XPR_UPS_GetString("/system/network/eth0/ipv4/netmask", value, &size) !=
+        0)
         printf("get net netmask error!\n");
     else
         printf("netmask = %s\n", value);
 
     size = sizeof(value);
-    if(XPR_UPS_GetString("/system/network/eth0/name", value, &size) != 0)
+    if (XPR_UPS_GetString("/system/network/eth0/name", value, &size) != 0)
         printf("get net eth0 name error!\n");
     else
         printf("ethernet name = %s\n", value);
 
     size = sizeof(value);
-    if(XPR_UPS_GetString("/system/information/internal-model", value, &size) != 0)
+    if (XPR_UPS_GetString("/system/information/internal-model", value, &size) !=
+        0)
         printf("get info internal-model error!\n");
     else
         printf("internal-model = %s\n", value);
 
     size = sizeof(value);
-    if(XPR_UPS_GetString("/system/network/ntp/serveradress", value, &size) != 0)
+    if (XPR_UPS_GetString("/system/network/ntp/serveradress", value, &size) !=
+        0)
         printf("get ntp serveradress error!\n");
     else
         printf("serveradress = %s\n", value);
 
     size = sizeof(value);
-    if(XPR_UPS_GetString("/system/network/eth0/ipv4/dns1", value, &size) != 0)
+    if (XPR_UPS_GetString("/system/network/eth0/ipv4/dns1", value, &size) != 0)
         printf("get net dns1 error!\n");
     else
         printf("dns1 = %s\n", value);
 
     size = sizeof(value);
-    if(XPR_UPS_GetString("/system/network/eth0/ipv4/dns2", value, &size) != 0)
+    if (XPR_UPS_GetString("/system/network/eth0/ipv4/dns2", value, &size) != 0)
         printf("get net dns2 error!\n");
     else
         printf("dns2 = %s\n", value);
 
-    int dhcp =0;
-    if(XPR_UPS_GetBoolean("/system/network/eth0/ipv4/dhcp", &dhcp) != 0)
+    int dhcp = 0;
+    if (XPR_UPS_GetBoolean("/system/network/eth0/ipv4/dhcp", &dhcp) != 0)
         printf("get net dhcp error!\n");
     else
         printf("dhcp = %d\n", dhcp);
 }
 
-int test_ups_get_set_string2()
-{
-	char value[128]={0};
-	int size = sizeof(value);
-
-	int err = 0;//XPR_UPS_SetStringVK(("192.168.1.18"), strlen("192.168.1.18"), "/system/network/eth0/ipv4/%s", "address");
-	printf("err = %d\n",err);
-   // err = XPR_UPS_GetString("/system/network/eth0/ipv4/address", value, &size);
-	//printf("err = %d, value=%s\n",err, value);
-    return 0;
-}
-
 int main(int argc, char* argv[])
 {
-	int result = 0;
+    int result = 0;
 
-	result = XPR_UPS_Init();
-	if(result!=0) {
-		printf("XPR_UPS_Init failed err = %d\n", result);
-		return 0;
-	}
+    result = XPR_UPS_Init("ups-storage.json");
+    if (result != 0) {
+        printf("XPR_UPS_Init() failed, error = 0x%08X\n", result);
+        return 1;
+    }
 
-	//test_ups_get_set_string();
+    test_XPR_UPS_Get_Set_String();
+    test_XPR_UPS_Get_SystemInformation();
+    test_XPR_UPS_Set_SystemInformation();
 
-   	test_ups_get_sys_info();
+    XPR_UPS_Fini();
 
-	XPR_UPS_Fini();
-	return 0;
+    return 0;
 }
