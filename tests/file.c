2ff5272e59523eb7f3693435253a7e797e228a72
Varphone Wong <varphone@qq.com>
2019-07-08 17:20:59 +0800

    tests: 新增 file 测试用例
    Signed-off-by: Varphone Wong <varphone@qq.com>

---
+++ tests/file.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <xpr/xpr_file.h>
+#include <xpr/xpr_mem.h>
+#include <xpr/xpr_utils.h>
+
+static void test_XPR_FilesInDir()
+{
+    char** list = NULL;
+    int files = XPR_FilesInDir(".", &list);
+    for (int i = 0; i < files; i++) {
+        printf("[%04d] \"%s\"\n", i, list[i]);
+    }
+    XPR_Freev((void**)(list));
+}
+
+int main(int argc, char** argv)
+{
+    test_XPR_FilesInDir();
+    return 0;
+}
