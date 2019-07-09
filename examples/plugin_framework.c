66186a5c731a9c082a8e83cb23699e162bca3690
Varphone Wong <varphone@qq.com>
2019-07-10 00:39:25 +0800

    examples/plugin_framework: 支持加载 DEBUG 编译的插件
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- examples/plugin_framework.c
+++ examples/plugin_framework.c
@@ -3,11 +3,18 @@
 #include <unistd.h>
 #include <xpr/xpr_plugin.h>
 
+#if defined(DEBUG) && defined(DEBUG_POSTFIX)
+#define NAMING "libplugin_%s" DEBUG_POSTFIX ".so"
+#else
+#define NAMING "libplugin_%s.so"
+#endif
+
 int main(int argc, char** argv)
 {
     XPR_PluginInit();
 #if 1
     XPR_PluginSetDirs("plugins;/usr/loca/plugins");
+    XPR_PluginSetNaming(NAMING);
     XPR_PluginLoad("c");
     XPR_PluginLoadDir("plugins");
     XPR_PluginLoadAll();
