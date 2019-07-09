612fe34e7e43e42b7d181e2107245d0b08e416a6
Varphone Wong <varphone@qq.com>
2019-07-09 23:52:52 +0800

    plugin: 实现卸载功能
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- src/xpr_plugin.c
+++ src/xpr_plugin.c
@@ -21,12 +21,12 @@
 struct XPR_PluginModule
 {
     XPR_ListNodeSL node;          ///< 链表节点
-    const char* libFileName;      ///< 插件模块的库文件
+    XPR_Atomic refcount;          ///< 引用计数
+    char* libFileName;            ///< 插件模块的库文件
     void* libHandle;              ///< 插件模块的库句柄
     void* pluginHandle;           ///< 插件句柄
     XPR_PluginModule* parent;     ///< 插件属主
     XPR_PluginRegistry* registry; ///< 插件信息
-    int numRegistries;            ///< 插件数量
     int rank;                     ///< 插件排位评分
 };
 
@@ -38,6 +38,10 @@ static XPR_List* sPluginList = NULL;
 
 #define XPR_PLUGIN_LOCK() XPR_RecursiveMutexLock(&sLock)
 #define XPR_PLUGIN_UNLOCK() XPR_RecursiveMutexUnlock(&sLock)
+
+#define MODULE_REF(m) XPR_AtomicInc(&(m)->refcount)
+#define MODULE_REFS(m) XPR_AtomicRead(&(m)->refcount)
+#define MODULE_UNREF(m) XPR_AtomicDec(&(m)->refcount)
 
 static void* nodeAlloc(void)
 {
@@ -57,9 +61,9 @@ int nodeCompare(void* a, void* b)
 {
     XPR_PluginModule* ma = (XPR_PluginModule*)(a);
     XPR_PluginModule* mb = (XPR_PluginModule*)(b);
-    if (ma->rank < mb->rank)
+    if (ma->refcount < mb->refcount)
         return -1;
-    if (ma->rank > mb->rank)
+    if (ma->refcount > mb->refcount)
         return 1;
     return 0;
 }
@@ -77,51 +81,91 @@ static XPR_PluginModule* findModule(const char* name)
 }
 
 struct FindContext {
-    const char* name;
-    char* pathBuf;
-    char* result;
+    const char* name;     // The name of the plugin
+    const char* fileName; // The name of file without path
+    char* pathBuf;        // The buffer to receive full path
+    char* result;         // Set to pathBuf is found
 };
+
+static int handleEachFile(void* opaque, const XPR_FileInfo* fileInfo)
+{
+    struct FindContext* ctx = (struct FindContext*)(opaque);
+    if (fileInfo->type == XPR_FILE_TYPE_REG) {
+        if (strcmp(ctx->fileName, fileInfo->name) == 0) {
+            strcpy(ctx->pathBuf, fileInfo->fullname);
+            ctx->result = ctx->pathBuf;
+            return XPR_FALSE;
+        }
+    }
+    return XPR_TRUE;
+}
 
 static void handleSegment(void* opaque, char* segment)
 {
     struct FindContext* ctx = (struct FindContext*)(opaque);
-    char basename[256];
-    char* result = NULL;
-    sprintf(basename, "plugin_%s.p", ctx->name);
-    char** list = NULL;
-    int numFiles = 0;
-    numFiles = XPR_FilesInDir(segment, &list);
-    if (numFiles > 0) {
-        for (int i = 0; i < numFiles; i++) {
-            if (strstr(list[i], basename)) {
-                strcpy(ctx->pathBuf, list[i]);
-                ctx->result = ctx->pathBuf;
-            }
-        }
-        XPR_Freev((void**)list);
-    }
+    XPR_FileForEach(segment, handleEachFile, ctx);
 }
 
 static char* findFileForName(const char* name, char* pathBuf)
 {
-    struct FindContext ctx = {name, pathBuf, NULL};
+    char fileName[256];
+    sprintf(fileName, "libplugin_%s.p", name);
+    struct FindContext ctx = {name, fileName, pathBuf, NULL};
     if (sCurrentDir[0])
         handleSegment(&ctx, sCurrentDir);
     if (ctx.result == NULL)
         xpr_foreach_s(sSerachDirs, -1, ";", handleSegment, &ctx);
     if (ctx.result == NULL) {
-        DBG(DBG_L2, "XPR_PLUGIN: The file of plugin \"%s\" does not exists",
+        DBG(DBG_L2, "XPR_PLUGIN: Plugin \"%s\" does not exists!",
             name);
     }
     return ctx.result;
 }
 
+// Return XPR_TRUE if the library has loaded
+static int isLibraryLoaded(const char* fileName)
+{
+    XPR_PluginModule* m = XPR_ListFirstNl(sPluginList);
+    while (m) {
+        if (m->libFileName) {
+            if (strcmp(fileName, m->libFileName) == 0)
+                return XPR_TRUE;
+        }
+        m = XPR_ListNextNl(sPluginList, m);
+    }
+    return XPR_FALSE;
+}
+
 // Forwards
 static int loadModuleDepends(const char** depends);
+static int unloadModule(XPR_PluginModule* m);
+static int unloadModuleByName(const char* name);
+static int unloadModuleDepends(const char** depends);
+
+static void refModuleRecursive(XPR_PluginModule* m)
+{
+    if (!m)
+        return;
+    // Add reference to module
+    MODULE_REF(m);
+    // Add reference to depends if exists
+    if (m->registry) {
+        const char** depends = m->registry->depends;
+        while (depends && *depends) {
+            refModuleRecursive(findModule(*depends));
+            depends++;
+        }
+    }
+}
 
 // Load plugin module with file name
 static int loadModule(const char* fileName)
 {
+    DBG(DBG_L5, "XPR_PLUGIN: Loading '%s' ...", fileName);
+    if (isLibraryLoaded(fileName)) {
+        DBG(DBG_L5, "XPR_PLUGIN: Plugin '%s' alread loaded!", fileName);
+        return XPR_ERR_OK;
+    }
     void* handle = dlopen(fileName, RTLD_LAZY);
     if (handle == NULL) {
         DBG(DBG_L1, "XPR_PLUGIN: dlopen(%s) failed, errno: %d", fileName,
@@ -143,40 +187,66 @@ static int loadModule(const char* fileName)
         return XPR_ERR_SYS(ENODATA);
     }
     int err = XPR_ERR_OK;
+    int loaded = 0;
     XPR_PluginModule* parent = NULL;
     if (numRegs > 1) {
         parent = nodeAlloc();
         parent->node.next = NULL;
         parent->libHandle = handle;
-        parent->libFileName = fileName;
+        parent->libFileName = XPR_StrDup(fileName);
         parent->parent = NULL;
-        parent->registry = regs;
-        parent->numRegistries = numRegs;
+        parent->registry = NULL;
+        MODULE_REF(parent);
         XPR_ListAppendNl(sPluginList, parent);
     }
-    for (int i = 0; i< numRegs; i++, regs++) {
-        if (regs->name == NULL)
+    XPR_PluginRegistry* reg = regs;
+    for (int i = 0; i< numRegs; i++, reg++) {
+        if (!reg->name)
             break;
-        if (regs->depends)
-            err = loadModuleDepends(regs->depends);
-        if (regs->init)
-            err = regs->init(regs);
-        if (err == XPR_ERR_OK) {
-            XPR_PluginModule* m = nodeAlloc();
-            if (m == NULL) {
-                DBG(DBG_L1, "XPR_PLUGIN: allocate memory failed！");
-                dlclose(handle);
-                return XPR_ERR_SYS(ENOMEM);
-            }
-            m->node.next = NULL;
-            m->libHandle = parent ? NULL : handle;
-            m->libFileName = parent ? NULL : fileName;
-            m->parent = parent;
-            m->registry = regs;
-            XPR_ListAppendNl(sPluginList, m);
-            DBG(DBG_L3, "XPR_PLUGIN: Plugin [%s, %s] loaded!", regs->name,
-                regs->version);
+        // Load all depends if exists
+        if (reg->depends)
+            err = loadModuleDepends(reg->depends);
+        if (err != XPR_ERR_OK)
+            break;
+        // Invoke initializer
+        if (reg->init)
+            err = reg->init(reg);
+        if (err != XPR_ERR_OK)
+            break;
+        DBG(DBG_L5, "XPR_PLUGIN: Plugin '%s' initialized!", reg->name);
+        // Add module to list
+        XPR_PluginModule* m = nodeAlloc();
+        if (m == NULL) {
+            DBG(DBG_L1, "XPR_PLUGIN: allocate memory failed!");
+            // Invoke finalizer
+            if (reg->fini)
+                reg->fini(reg);
+            break;
         }
+        m->node.next = NULL;
+        m->libHandle = parent ? NULL : handle;
+        m->libFileName = parent ? NULL : XPR_StrDup(fileName);
+        m->parent = parent;
+        m->registry = reg;
+        if (parent)
+            MODULE_REF(parent);
+        MODULE_REF(m);
+        XPR_ListAppendNl(sPluginList, m);
+        DBG(DBG_L3, "XPR_PLUGIN: Plugin [%s, %s] loaded!", reg->name,
+            reg->version);
+        // Move to next registry
+        loaded++;
+    }
+    // Cleanup if not success
+    if (err != XPR_ERR_OK) {
+        reg = regs;
+        for (int i = 0; i< loaded; i++, reg++) {
+            if (reg->name == NULL)
+                break;
+            unloadModuleByName(reg->name);
+        }
+        if (parent && loaded == 0)
+            unloadModule(parent);
     }
     return err;
 }
@@ -188,10 +258,17 @@ static int loadModuleDepends(const char** depends)
         return XPR_ERR_OK;
     int err = XPR_ERR_OK;
     char path[PATH_MAX];
+    XPR_PluginModule* m = NULL;
     while (*depends) {
+        DBG(DBG_L5, "XPR_PLUGIN: Loading depends '%s' ...", *depends);
         // Skip if already loaded
-        if (findModule(*depends) != NULL)
+        m = findModule(*depends);
+        if (m) {
+            DBG(DBG_L5, "XPR_PLUGIN: Load depends '%s' ignored!",
+                *depends);
             goto next;
+        }
+        // Find the fullname of the plugin matched to the depends
         if (findFileForName(*depends, path) == NULL) {
             err = XPR_ERR_SYS(ENOENT);
             break;
@@ -200,10 +277,82 @@ static int loadModuleDepends(const char** depends)
         err = loadModule(path);
         if (err != XPR_ERR_OK)
             break;
+        DBG(DBG_L5, "XPR_PLUGIN: Load depends '%s' okay!", *depends);
+        m = findModule(*depends);
 next:
+        refModuleRecursive(m);
         depends++;
     }
     return err;
+}
+
+// Unload the module and remove the list
+static int unloadModule(XPR_PluginModule* m)
+{
+    if (!m)
+        return XPR_ERR_GEN_NULL_PTR;
+    // Finalize the module befor actual unload
+    if (m->registry) {
+        if (MODULE_REFS(m) == 1) {
+            if (m->registry->fini) {
+                int err = m->registry->fini(m->registry);
+                if (err != XPR_ERR_OK) {
+                    DBG(DBG_L2,
+                        "XPR_PLUGIN: Finalize '%s' failed, errno: 0x%08X",
+                        m->registry->name, err);
+                    return XPR_ERR_GEN_BUSY;
+                }
+                DBG(DBG_L5, "XPR_PLUGIN: Plugin '%s' finalized!",
+                    m->registry->name);
+            }
+            DBG(DBG_L5, "XPR_PLUGIN: Unloading '%s' @ %p ...",
+                m->registry->name, m);
+            // Unload all depends
+            if (m->registry->depends)
+                unloadModuleDepends(m->registry->depends);
+        }
+    }
+    // Unload now if refs == 0
+    if (MODULE_UNREF(m) == 0) {
+        if (m->libFileName)
+            XPR_Free(m->libFileName);
+        if (m->libHandle)
+            dlclose(m->libHandle);
+        // Unload parent if exists
+        if (m->parent) {
+            if (MODULE_UNREF(m->parent) == 1)
+                unloadModule(m->parent);
+        }
+        XPR_ListRemoveNl(sPluginList, m);
+        DBG(DBG_L5, "XPR_PLUGIN: Unload %p okay!", m);
+    }
+    return XPR_ERR_OK;
+}
+
+// Unload module matched to name
+static int unloadModuleByName(const char* name)
+{
+    if (!name)
+        return XPR_ERR_GEN_NULL_PTR;
+    XPR_PluginModule* m = findModule(name);
+    if (!m)
+        return XPR_ERR_SYS(ENOENT);
+    return unloadModule(m);
+}
+
+// Unload modules matched to name list
+static int unloadModuleDepends(const char** depends)
+{
+    XPR_PluginModule* m = NULL;
+    while (*depends) {
+        m = findModule(*depends);
+        if (m) {
+            if (MODULE_UNREF(m) == 1)
+                unloadModule(m);
+        }
+        depends++;
+    }
+    return XPR_ERR_OK;
 }
 
 XPR_API int XPR_PluginInit(void)
@@ -300,8 +449,29 @@ XPR_API int XPR_PluginLoadDir(const char* dir)
 
 XPR_API int XPR_PluginUnload(const char* name)
 {
-    // FIXME:
-    return XPR_ERR_GEN_NOT_SUPPORT;
+    int err = XPR_ERR_OK;
+    XPR_PLUGIN_LOCK();
+    err = unloadModuleByName(name);
+    XPR_PLUGIN_UNLOCK();
+    return err;
+}
+
+XPR_API int XPR_PluginUnloadAll(void)
+{
+    int err = XPR_ERR_OK;
+    XPR_PLUGIN_LOCK();
+    // Sort all module by refcount (asc)
+    XPR_ListSortNl(sPluginList, NULL);
+    XPR_PluginModule* m = XPR_ListFirstNl(sPluginList);
+    while (m) {
+        unloadModule(m);
+        // The list is changed, sort again
+        XPR_ListSortNl(sPluginList, NULL);
+        // Peek next module
+        m = XPR_ListFirstNl(sPluginList);
+    }
+    XPR_PLUGIN_UNLOCK();
+    return err;
 }
 
 XPR_API void* XPR_PluginFind(const char* name)
