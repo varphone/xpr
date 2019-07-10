70f7c18592240433ac5ff3602346bcf847192e79
Varphone Wong <varphone@qq.com>
2019-07-10 15:42:01 +0800

    tests/file.c: 新增 XPR_FileCopy 测试例程
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- tests/file.c
+++ tests/file.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <xpr/xpr_file.h>
 #include <xpr/xpr_mem.h>
+#include <xpr/xpr_sys.h>
 #include <xpr/xpr_utils.h>
 
 static void test_XPR_FilesInDir()
@@ -37,9 +38,27 @@ static void test_XPR_FileForEach()
     XPR_FileForEach("/usr/lib/", forEachFile, NULL);
 }
 
+static void test_XPR_FileCopy()
+{
+    printf("test_XPR_FileCopy 1\n");
+    XPR_File* f = XPR_FileOpen("test-file-copy.txt", "cwt");
+    char buf[1024];
+    int n = snprintf(buf, sizeof(buf), "Test, CTS = %ld\n", XPR_SYS_GetCTS());
+    XPR_FileWrite(f, (const uint8_t*)(buf), n);
+    XPR_FileClose(f);
+    printf("test_XPR_FileCopy 2\n");
+    XPR_FileCopy("test-file-copy.txt.2", "test-file-copy.txt.3");
+    printf("test_XPR_FileCopy 3\n");
+    XPR_FileCopy("test-file-copy.txt.1", "test-file-copy.txt.2");
+    printf("test_XPR_FileCopy 4\n");
+    XPR_FileCopy("test-file-copy.txt", "test-file-copy.txt.1");
+    printf("test_XPR_FileCopy 5\n");
+}
+
 int main(int argc, char** argv)
 {
     test_XPR_FilesInDir();
     test_XPR_FileForEach();
+    test_XPR_FileCopy();
     return 0;
 }
